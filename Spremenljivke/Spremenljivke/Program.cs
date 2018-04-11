2ac20f8469d34196a16f9b406f94ad946b2f9e25
B23 <B23@c.scng.si>
2018-04-11 11:25:20 +0200

    Začetni commit

---
+++ Spremenljivke/Spremenljivke/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spremenljivke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int število1;
+            število1 = 12;
+            int število2 = 13;
+            //  Console.WriteLine("Vsota je "+(število1+število2));
+            Console.Write("Vsota je ");
+            Console.WriteLine(število1+število2);
+            byte nekaj = 17;
+            long dolgoŠtevilo = 200000000000;
+            int pozitivno = -2;
+            double realno = 2.5;
+            float enojna = 2.5f;
+            decimal x = 2.5m;
+            char znak= '?';
+            bool jeRes = false;
+            string beseda = "to je lahko en niz"; // ni enostaven tip
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
